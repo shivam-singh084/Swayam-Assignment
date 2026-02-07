cd84221c99f8030d5b81f1d079a0110d23369a8f
Shivam Kumar Singh <aids23044@glbitm.ac.in>
2026-02-07 22:32:13 +0530

    Time: 0 ms (100%), Space: 9.6 MB (91.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(begin(s), end(s));
-        sort(begin(t), end(t));
-
-        return s==t;
+        int freq[26] = {0};
+        for(int i = 0; i < s.length(); i++){
+            freq[s[i] - 'a']++;
+        }
+        for(int i = 0; i < t.length(); i++){
+            freq[t[i] - 'a']--;
+        }
+        for(int i = 0; i < 26; i++){
+            if(freq[i] != 0){
+                return false;
+            }
+        }
+        return true;
     }
 };
